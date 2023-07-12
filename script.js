bf545b22369b18fe9f67c958180ddcae520df8c8
Dennis Corsi <denniscorsi@gmail.com>
2023-07-12 14:44:00 -0400

    added test form

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function sayHi(){
 //alert("Hello!");
 document.getElementById("emptyDiv").innerText = "Hello!!"
 }
+
+
+
+document.getElementById("ageForm"). addEventListener("submit", calculateYear);
+
+function calculateYear(event){
+    const formData = new FormData(event.target);
+    const birthYear = formData.get("year")
+    const age = 2023-birthYear;
+    document.getElementById("age").innerText = age;
+}
+
